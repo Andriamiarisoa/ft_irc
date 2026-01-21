ed9e0f6fdb824f9f89e74126493010eb8f257561
Baholinirina Raelinomena <raelinomena@gmail.com>
2026-01-21 06:27:35 +0000

    feat: gestion d' erreur pour le parsing des messages

--- src/MessageParser.cpp
+++ src/MessageParser.cpp
@@ -3,58 +3,57 @@
 #include "../includes/Server.hpp"
 #include "../includes/Client.hpp"
 #include <string>
+#include <exception>
 
 Command* createCommand(const std::string& cmd, Server* srv, Client* cli,
         const std::vector<std::string>& params)
 {
-    if (cmd == "INVITE") {
-        return new InviteCommand(srv, cli, params);
-    }
-    else if (cmd == "JOIN") {
-        return new JoinCommand(srv, cli, params);
-    }
-    else if (cmd == "KICK") {
-        return new KickCommand(srv, cli, params);
-    }
-    else if (cmd == "MODE") {
-        return new ModeCommand(srv, cli, params);
-    }
-    else if (cmd == "NICK") {
-        return new NickCommand(srv, cli, params);
-    }
-    else if (cmd == "PART") {
-        return new PartCommand(srv, cli, params);
-    }
-    else if (cmd == "PASS") {
-        return new PassCommand(srv, cli, params);
-    }
-    else if (cmd == "PRIVMSG") {
-        return new PrivmsgCommand(srv, cli, params);
-    }
-    else if (cmd == "QUIT") {
-        return new QuitCommand(srv, cli, params);
-    }
-    else if (cmd == "TOPIC") {
-        return new TopicCommand(srv, cli, params);
-    }
-    else if (cmd == "USER") {
-        return new UserCommand(srv, cli, params);
-    }
-    else
-        return NULL;
-}
-Command* MessageParser::parse(const std::string& line, Server* srv, Client* cli) {
-    std::vector<std::string>    params;
-    std::string                 cmd;
-
-    params = splitParams(line);
     if (params.empty())
+    {
+        std::cerr << "Missing parameters for command: " << cmd << std::endl;
         return NULL;
-    cmd = params.front();
-    params.erase(params.begin());
-    for (int i = 0; i < cmd.size(); i++)
-        cmd[i] = toupper(cmd[i]);
-    return createCommand(cmd, srv, cli, params);
+    }
+    try{
+        if (cmd == "INVITE") {
+            return new InviteCommand(srv, cli, params);
+        }
+        else if (cmd == "JOIN") {
+            return new JoinCommand(srv, cli, params);
+        }
+        else if (cmd == "KICK") {
+            return new KickCommand(srv, cli, params);
+        }
+        else if (cmd == "MODE") {
+            return new ModeCommand(srv, cli, params);
+        }
+        else if (cmd == "NICK") {
+            return new NickCommand(srv, cli, params);
+        }
+        else if (cmd == "PART") {
+            return new PartCommand(srv, cli, params);
+        }
+        else if (cmd == "PASS") {
+            return new PassCommand(srv, cli, params);
+        }
+        else if (cmd == "PRIVMSG") {
+            return new PrivmsgCommand(srv, cli, params);
+        }
+        else if (cmd == "QUIT") {
+            return new QuitCommand(srv, cli, params);
+        }
+        else if (cmd == "TOPIC") {
+            return new TopicCommand(srv, cli, params);
+        }
+        else if (cmd == "USER") {
+            return new UserCommand(srv, cli, params);
+        }
+    }
+    catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return NULL;
+    }
+    std::cerr << "Unknown command: " << cmd << std::endl;
+    return NULL;
 }
 
 std::string MessageParser::extractPrefix(const std::string& line) {
@@ -68,7 +67,7 @@ std::string MessageParser::extractPrefix(const std::string& line) {
     return line.substr(0, pos);
 }
 
-std::string prepareCommandLine(const std::string& line) {
+std::string removePrefix(const std::string& line) {
     std::string prefix;
     std::string cmdLine;
     size_t      pos;
@@ -92,7 +91,7 @@ std::vector<std::string> MessageParser::splitParams(const std::string& str) {
     std::string                 cmdLine;
     size_t                      pos;
 
-    cmdLine = prepareCommandLine(str);
+    cmdLine = removePrefix(str);
     if (cmdLine.empty())
         return std::vector<std::string>();
     while (!cmdLine.empty() && cmdLine[0] != ':')
@@ -106,6 +105,28 @@ std::vector<std::string> MessageParser::splitParams(const std::string& str) {
         cmdLine = cmdLine.substr(pos, cmdLine.size() - pos);
     }
     if (!cmdLine.empty())
-        result.push_back(cmdLine);
+    {
+        if (cmdLine[0] == ' ')
+            result.clear();
+        else
+            result.push_back(cmdLine);
+    }
     return result;
+}
+
+Command* MessageParser::parse(const std::string& line, Server* srv, Client* cli) {
+    std::vector<std::string>    params;
+    std::string                 cmd;
+
+    params = splitParams(line);
+    if (params.empty())
+    {
+        std::cerr << "Invalid command" << std::endl;
+        return NULL;
+    }
+    cmd = params.front();
+    params.erase(params.begin());
+    for (int i = 0; i < cmd.size(); i++)
+        cmd[i] = std::toupper(cmd[i]);
+    return createCommand(cmd, srv, cli, params);
 }
