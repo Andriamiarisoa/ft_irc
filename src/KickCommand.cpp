63e3f57402a413728142002cc5ddd5fac1ddb814
IAnero <rakotondrianaheritianafitiavan@gmail.com>
2026-01-22 21:42:40 +0000

    feat: start of func KICK

--- src/KickCommand.cpp
+++ src/KickCommand.cpp
@@ -1,4 +1,8 @@
 #include "../includes/KickCommand.hpp"
+#include "../includes/Client.hpp"
+#include "../includes/Server.hpp"
+#include "../includes/Channel.hpp"
+
 
 KickCommand::KickCommand(Server* srv, Client* cli, const std::vector<std::string>& params)
     : Command(srv, cli, params) {
@@ -8,4 +12,22 @@ KickCommand::~KickCommand() {
 }
 
 void KickCommand::execute() {
+    if (!client->isRegistered()) {
+        sendError(451, ": You have not registered");
+        return;
+    }
+
+    if (params.size() < 2) {
+        sendError(461, "KICK :Not enough parameters");
+        return;
+    }
+
+    std::string channelName = params[0];
+    std::string targetname = params[1];
+    if (params.size() == 3) {
+        std::string reason = params[2];
+    }
+
+    Channel* channel = server.getChanne
+
 }
