bada2f1055ca50909cf68b4c5b2badb11c2404de
Yasser Andriamiarisoa <andriamiarisoa.yasser@gmail.com>
2026-01-22 08:16:53 +0000

    refact: correcting code format for better readability as per Baholy's suggestion

--- src/NickCommand.cpp
+++ src/NickCommand.cpp
@@ -10,33 +10,42 @@ NickCommand::NickCommand(Server* srv, Client* cli, const std::vector<std::string
 NickCommand::~NickCommand() {
 }
 
-void NickCommand::execute() {
+bool NickCommand::checkErrors() {
     std::string newNick = params.empty() ? "" : params[0];
     std::string currentNick = client->getNickname();
 
     if (!client->isAuthenticated()) {
         sendError(464, ":You must send PASS first");
-        return;
+        return true;
     }
     if (params.empty() || params.size() < 1 || params[0].empty()) {
         sendError(431, ":No nickname given");
-        return;
+        return true;
     }
     if (server->getClientByNick(newNick) != NULL && newNick != currentNick) {
         sendError(433, newNick + " :Nickname is already in use");
-        return;
+        return true;
     }
     client->setNickname(newNick);
     if (client->getNickname() != newNick) {
         sendError(432, newNick + " :Erroneous nickname");
-        return;
+        return true;
+    }
+    return false;
+}
+
+void NickCommand::execute() {
+    std::string newNick = params.empty() ? "" : params[0];
+    std::string currentNick = client->getNickname();
+
+    if (checkErrors()) {
+        return ;
     }
     if (client->isRegistered()) {
         std::string msg = ":" + currentNick + "!" + client->getUsername() + "@" + client->getHostname() + " NICK :" + newNick;
-        client->sendMessage(msg);
-        
-        // Broadcast to all channels the client is a member of
         const std::set<Channel*>& channels = client->getChannels();
+
+        client->sendMessage(msg);
         for (std::set<Channel*>::const_iterator it = channels.begin(); it != channels.end(); ++it) {
             (*it)->broadcast(msg, client);
         }
@@ -46,12 +55,9 @@ void NickCommand::execute() {
         std::string version = "1.0";
 
         client->registerClient();
-        sendReply(001, ":Welcome to the IRC Network " + client->getNickname() + "!" +
-            client->getUsername() + "@" + client->getHostname());
-        sendReply(002, ":Your host is " + serverName +
-            ", running version " + version);
+        sendReply(001, ":Welcome to the IRC Network " + client->getNickname() + "!" + client->getUsername() + "@" + client->getHostname());
+        sendReply(002, ":Your host is " + serverName + ", running version " + version);
         sendReply(003, ":This server was created just now");
-        sendReply(004, serverName + " " + version +
-            " o o");
+        sendReply(004, serverName + " " + version + " o o");
     }
 }
